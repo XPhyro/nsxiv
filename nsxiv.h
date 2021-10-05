9c821c6a94a9c19e104d4d22df30921f38a3710e
Reto Schnyder <reto.a.schnyder@bluewin.ch>
2021-10-05 20:34:41 +0200

    Enable background transparency
    Add the Xresource .window.alpha, which should be a float between 0.0 and 1.0 and sets the background
    transparency of the window.
    
    The main rendering code is factored out to `render_core()` in `image.c`, so it can be reused for
    thumbnail rendering.

--- nsxiv.h
+++ nsxiv.h
@@ -218,10 +218,7 @@ void img_update_color_modifiers(img_t*);
 bool img_change_color_modifier(img_t*, int, int*);
 bool img_frame_navigate(img_t*, int);
 bool img_frame_animate(img_t*);
-Imlib_Image img_open(const fileinfo_t*);
-#if HAVE_LIBEXIF
-void exif_auto_orientate(const fileinfo_t*);
-#endif
+void render_core(win_t*, int, int, int, int, int, int, int, int, bool);
 
 
 /* options.c */
@@ -385,6 +382,8 @@ struct win {
 	XColor win_bg;
 	XColor win_fg;
 	XColor mrk_fg;
+	XColor win_bg_premul; /* premultiplied alpha */
+	unsigned int win_alpha;
 #if HAVE_LIBFONTS
 	XftColor bar_bg;
 	XftColor bar_fg;
