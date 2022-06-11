74825b50d80791b699510db13b548bcb0cee302d
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2022-06-11 15:56:42 +0300

    Fix compilation errors on master#09d2d738ad4beea92

--- window.c
+++ window.c
@@ -115,6 +115,7 @@ void win_init(win_t *win)
 	XVisualInfo vis;
 	float alpha;
 	char *endptr;
+	static char lbuf[512 + 3], rbuf[64 + 3]; /* needed for transparency */
 
 	memset(win, 0, sizeof(win_t));
 
