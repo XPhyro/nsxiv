1ad7a307a42224c97b7498de4e1db0bcef59b765
NRK <nrk@disroot.org>
2022-10-04 13:26:07 +0600

    img_render: explicitly set blending
    this was the cause of the transparency bug for animated pngs.
    img_load_multiframe() was messing with the blend flag and thus it messed
    with the rendering function.
    
    the fix is to explicitly enable blending inside the rendering function.
    
    also explicitly set operation as well just in case some other place in
    the codebase messes with it in the future.
    
    Closes: https://codeberg.org/nsxiv/nsxiv/issues/300

--- image.c
+++ image.c
@@ -768,6 +768,8 @@ void img_render(img_t *img)
 			imlib_context_set_color(c.red >> 8, c.green >> 8, c.blue >> 8, 0xFF);
 			imlib_image_fill_rectangle(0, 0, dw, dh);
 		}
+		imlib_context_set_blend(1);
+		imlib_context_set_operation(IMLIB_OP_COPY);
 		imlib_blend_image_onto_image(img->im, 0, sx, sy, sw, sh, 0, 0, dw, dh);
 		imlib_context_set_color_modifier(NULL);
 		imlib_render_image_on_drawable(dx, dy);
